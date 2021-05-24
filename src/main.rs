8ccd330138de5fbf39c4bd6958aa080f6f820941
Andrew Lee <candrewlee14@gmail.com>
2021-05-24 00:56:49 -0600

    fix read_clients permanent read lock with timeout, change to mutex

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use anyhow::{anyhow, bail, Context, Result};
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 use std::{collections::HashMap, net::SocketAddr};
-use tokio::io::{self, AsyncReadExt, AsyncWriteExt, Error};
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt, Error, ReadHalf, WriteHalf};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::{RwLock, RwLockWriteGuard};
+use tokio::sync::{Mutex, RwLock};
 use tokio::task::JoinHandle;
 use tokio::time::{interval, Duration};
-use tracing::{event, instrument, span, Level};
+use tracing::{event, instrument, Level};
 use tracing_subscriber;
 
 #[allow(dead_code)]
@@ -23,9 +23,11 @@ impl Player {
         Self { name, x: 0, y: 0 }
     }
 }
-type AtomicStream = Arc<RwLock<TcpStream>>;
+type AtomicStream = Arc<Mutex<TcpStream>>;
+type AtomicReadStream = Arc<Mutex<ReadHalf<TcpStream>>>;
+type AtomicWriteStream = Arc<Mutex<WriteHalf<TcpStream>>>;
 type AtomicPlayer = Arc<RwLock<Player>>;
-type AtomicVec<T> = Arc<RwLock<Vec<T>>>;
+type AtomicVec<T> = Arc<Mutex<Vec<T>>>;
 type AtomicHashMap<K, V> = Arc<RwLock<HashMap<K, V>>>;
 
 #[instrument(skip(players))]
@@ -59,20 +61,21 @@ async fn atomic_hashmap_to_string(
     Ok(players_buf)
 }
 
-#[instrument(skip(streams, players))]
+#[instrument(skip(read_streams, write_streams, players))]
 async fn process(
-    streams: AtomicVec<AtomicStream>,
+    read_streams: AtomicVec<AtomicReadStream>,
+    write_streams: AtomicVec<AtomicWriteStream>,
     players: AtomicHashMap<SocketAddr, AtomicPlayer>,
 ) -> Result<()> {
     let mut interval = interval(Duration::from_millis(1000));
     loop {
         interval.tick().await;
         let players_serialized = atomic_hashmap_to_string(players.clone()).await?;
-        let read_streams = streams.read().await;
-        event!(Level::TRACE, "Writing to players");
-        for (i, stream) in read_streams.iter().enumerate() {
+        let writeable_streams = write_streams.lock().await;
+        event!(Level::TRACE, "Writing to players, lock obtained");
+        for (i, stream) in writeable_streams.iter().enumerate() {
             event!(Level::TRACE, "Waiting for access to for stream write");
-            let mut editable_stream = stream.write().await;
+            let mut editable_stream = stream.lock().await;
             event!(Level::TRACE, "Obtained lock for stream write");
             match tokio::time::timeout(
                 Duration::from_millis(25),
@@ -80,34 +83,46 @@ async fn process(
             )
             .await
             {
-                Err(_) => {
-                    event!(Level::DEBUG, "Client write timed out");
+                Ok(Ok(_)) => {}
+                Ok(Err(_)) => {
+                    event!(Level::WARN, "Client write failed");
                 }
-                _ => {}
+                Err(_) => {
+                    event!(Level::WARN, "Client write timed out");
+                }
             };
-            event!(Level::DEBUG, "Wrote to client {}", i);
+            event!(Level::TRACE, "Wrote to client {}", i);
         }
-        event!(Level::TRACE, "Finished writing to players");
+        event!(Level::DEBUG, "Finished writing to players");
     }
 }
 
-#[instrument(skip(streams, players))]
+#[instrument(skip(read_streams, write_streams, players))]
 async fn read_clients(
-    streams: AtomicVec<AtomicStream>,
+    read_streams: AtomicVec<AtomicReadStream>,
+    write_streams: AtomicVec<AtomicWriteStream>,
     players: AtomicHashMap<SocketAddr, AtomicPlayer>,
 ) -> Result<()> {
     let mut interval = interval(Duration::from_millis(250));
     loop {
         interval.tick().await;
         event!(Level::TRACE, "Reading Streams");
-        let read_streams = streams.read().await;
-        for stream in read_streams.iter() {
-            let mut editable_stream = stream.write().await;
+        let readable_streams = read_streams.lock().await;
+        for stream in readable_streams.iter() {
+            let mut editable_stream = stream.lock().await;
             let mut recv = String::new();
-            match (*editable_stream).read_to_string(&mut recv).await {
-                Ok(_) => {}
+            match tokio::time::timeout(
+                Duration::from_millis(25),
+                (*editable_stream).read_to_string(&mut recv),
+            )
+            .await
+            {
+                Ok(Ok(_)) => {}
+                Ok(Err(_)) => {
+                    event!(Level::WARN, "Client read error")
+                }
                 Err(_) => {
-                    event!(Level::WARN, "Stream Read Error");
+                    event!(Level::TRACE, "Client read timed out")
                 }
             };
         }
@@ -115,27 +130,29 @@ async fn read_clients(
     }
 }
 
-#[instrument(skip(listener, streams, players))]
+#[instrument(skip(listener, read_streams, write_streams, players))]
 async fn accept_connections(
     listener: TcpListener,
-    streams: AtomicVec<AtomicStream>,
+    read_streams: AtomicVec<AtomicReadStream>,
+    write_streams: AtomicVec<AtomicWriteStream>,
     players: AtomicHashMap<SocketAddr, AtomicPlayer>,
 ) -> Result<()> {
-    let mut interval = interval(Duration::from_millis(250));
     loop {
-        interval.tick().await;
         event!(Level::TRACE, "Looking for new connections");
         match listener.accept().await {
             Ok((mut stream, addr)) => {
                 event!(Level::INFO, "Client connected at address {:?}", addr);
-                stream.write_all(b"yo welcome\n").await?;
+                stream.write_all(b"welcome!\n").await?;
+                let (read_stream, write_stream) = io::split(stream);
                 {
-                    event!(Level::TRACE, "Waiting for access to for stream write");
-                    let mut mutable_streams = streams.write().await;
-                    (*mutable_streams).push(Arc::new(RwLock::new(stream)));
-                    event!(Level::TRACE, "Stream write complete");
-                }
-                {
+                    event!(Level::DEBUG, "Waiting for access to for write_stream write");
+                    let mut mutable_write_streams = write_streams.lock().await;
+                    (*mutable_write_streams).push(Arc::new(Mutex::new(write_stream)));
+                    event!(Level::DEBUG, "Stream write complete");
+                    event!(Level::DEBUG, "Waiting for access to for read_stream write");
+                    let mut mutable_read_streams = read_streams.lock().await;
+                    event!(Level::DEBUG, "Obtained lock for read_stream write");
+                    (*mutable_read_streams).push(Arc::new(Mutex::new(read_stream)));
                     event!(Level::TRACE, "Waiting for access to for players write");
                     let mut mutable_map = players.write().await;
                     (*mutable_map).insert(
@@ -157,35 +174,51 @@ async fn main() -> Result<()> {
     let format = tracing_subscriber::fmt::format().without_time();
     let subscriber = tracing_subscriber::fmt()
         .event_format(format)
-        .with_max_level(Level::TRACE)
+        .with_max_level(Level::DEBUG)
         .finish();
     tracing::subscriber::set_global_default(subscriber)?;
 
     event!(Level::INFO, "Server started!");
     let listener = TcpListener::bind("127.0.0.1:5000").await?;
-    let streams: AtomicVec<AtomicStream> = Arc::new(RwLock::new(Vec::new()));
+    let write_streams: AtomicVec<AtomicWriteStream> = Arc::new(Mutex::new(Vec::new()));
+    let read_streams: AtomicVec<AtomicReadStream> = Arc::new(Mutex::new(Vec::new()));
     let players: AtomicHashMap<SocketAddr, AtomicPlayer> = Arc::new(RwLock::new(HashMap::new()));
-    let streams_clone = streams.clone();
-    let players_clone = players.clone();
-    let connection_thread = tokio::spawn(async move {
-        let streams_arc = streams_clone.clone();
-        let players_arc = players_clone.clone();
-        accept_connections(listener, streams_arc, players_arc).await
-    });
-    let streams_clone2 = streams.clone();
-    let players_clone2 = players.clone();
-    let process_thread = tokio::spawn(async move {
-        let streams_arc = streams_clone2.clone();
-        let players_arc = players_clone2.clone();
-        process(streams_arc, players_arc).await
-    });
-    let streams_clone3 = streams.clone();
-    let players_clone3 = players.clone();
-    let read_thread = tokio::spawn(async move {
-        let streams_arc = streams_clone3.clone();
-        let players_arc = players_clone3.clone();
-        read_clients(streams_arc, players_arc).await
-    });
+    let connection_thread = {
+        // Accept Connections
+        let read_streams_clone = read_streams.clone();
+        let write_streams_clone = write_streams.clone();
+        let players_clone = players.clone();
+        tokio::spawn(async move {
+            let read_streams_arc = read_streams_clone.clone();
+            let write_streams_arc = write_streams_clone.clone();
+            let players_arc = players_clone.clone();
+            accept_connections(listener, read_streams_arc, write_streams_arc, players_arc).await
+        })
+    };
+    let process_thread = {
+        // Process Connections
+        let read_streams_clone = read_streams.clone();
+        let write_streams_clone = write_streams.clone();
+        let players_clone = players.clone();
+        tokio::spawn(async move {
+            let read_streams_arc = read_streams_clone.clone();
+            let write_streams_arc = write_streams_clone.clone();
+            let players_arc = players_clone.clone();
+            process(read_streams_arc, write_streams_arc, players_arc).await
+        })
+    };
+    let read_thread = {
+        // Read from Client Connections
+        let read_streams_clone = read_streams.clone();
+        let write_streams_clone = write_streams.clone();
+        let players_clone = players.clone();
+        tokio::spawn(async move {
+            let read_streams_arc = read_streams_clone.clone();
+            let write_streams_arc = write_streams_clone.clone();
+            let players_arc = players_clone.clone();
+            read_clients(read_streams_arc, write_streams_arc, players_arc).await
+        })
+    };
     match tokio::try_join!(connection_thread, process_thread, read_thread) {
         Ok(_) => (),
         Err(e) => println!("Error: {:?}", e),
