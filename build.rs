e15f4f738c663144950f5ced54420dd5d65b30c4
Andrew Lee <candrewlee14@gmail.com>
2021-12-28 14:59:18 -0700

    remove vestigial files

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    tonic_build::compile_protos("proto/mazeio.proto")
-        .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
-    // tonic_build::configure()
-    // .build_client(false)
-    // .out_dir("another_crate/src/pb")
-    // .compile(&["proto/mazeio.proto"], &["proto"])
-    // .expect("failed to compile protos");
-}
